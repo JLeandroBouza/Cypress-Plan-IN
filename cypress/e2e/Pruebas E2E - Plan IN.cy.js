c631a8b3fffd6f40126b8d85712003905f676c7a
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-12-12 21:56:30 -0300

    Creacion del proyecto, configuracion del framework

---
+++ cypress/e2e/Pruebas E2E - Plan IN.cy.js
@@ -0,0 +1,11 @@
+
+describe('Pruebas E2E sobre Aplicación Web Plant - IN', () => {
+
+  beforeEach (function () {
+    cy.visit('')
+  });
+  
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
