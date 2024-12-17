6c3652b25b3650f4f47e6876b3d072e65329257d
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-12-16 23:50:07 -0300

    Se realiza los selectores por medio de Xpath, se importa la clase homepage

--- cypress/e2e/Pruebas E2E - Plan IN.cy.js
+++ cypress/e2e/Pruebas E2E - Plan IN.cy.js
@@ -1,3 +1,4 @@
+import { homepage } from "../pages/home";
 
 describe('Pruebas E2E sobre Aplicación Web Plant - IN', () => {
 
@@ -5,7 +6,15 @@ describe('Pruebas E2E sobre Aplicación Web Plant - IN', () => {
     cy.visit('')
   });
   
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Validar el ingreso correcto a la web de Plant - IN', () => {
+    
+    homepage.validarHome();
   })
+
+  it('Validar elementos visibles en el Home', () =>{
+
+    homepage.validarElementosHome();
+
+  })
+
 })
---
+++ cypress/pages/home.js
@@ -0,0 +1,41 @@
+export class homePage {
+
+    constructor(){
+        this.botonIngresar = '//*[@id="root"]/div/header/div[1]/div[3]/nav/ul/li[1]/a/button'
+        this.botonRegistrate= '//*[@id="root"]/div/header/div[1]/div[3]/nav/ul/li[2]/a/button'
+        this.botonHome = '//*[@id="root"]/div/header/div[2]/nav/ul/li[1]/a/button'
+        this.botonPublicaciones = '//*[@id="root"]/div/header/div[2]/nav/ul/li[2]/button'
+        this.botonForo = '//*[@id="root"]/div/header/div[2]/nav/ul/li[3]/button'
+        this.SomoCartel = '//*[@id="root"]/main/div[1]/div/div[1]/h2'
+        this.registroModal = '//*[@id="root"]/main/div[3]/div/a/button'
+        this.testimonios = '//*[@id="root"]/main/div[4]/div/h2'
+        this.cardTestimonios = '//*[@id="root"]/main/div[4]/div/div/div/div[2]'
+        this.botonInstagram = 'a[aria-label="Compartir en Instagram"]'
+        this.botonFacebook = 'a[aria-label="Compartir en Facebook"]'
+        this.botonLinkedin = 'a[aria-label="Compartir en Linkedin"]'
+        this.botonTwitter = 'a[aria-label="Compartir en Twitter"]'
+    }
+
+    validarHome(){
+        cy.url().should('eq', 'https://plant-in.netlify.app/')
+    }
+
+    validarElementosHome(){
+        cy.xpath(this.botonIngresar).should('be.visible')
+        cy.xpath(this.botonRegistrate).should('be.visible')
+        cy.xpath(this.botonHome).should('be.visible')
+        cy.xpath(this.botonPublicaciones).should('be.visible')
+        cy.xpath(this.botonForo).should('be.visible')
+        cy.xpath(this.SomoCartel).should('be.visible')
+        cy.xpath(this.registroModal).should('be.visible')
+        cy.xpath(this.testimonios).should('be.visible')
+        cy.xpath(this.cardTestimonios).should('be.visible')
+        cy.get(this.botonInstagram).should('be.visible')
+        cy.get(this.botonFacebook).should('be.visible')
+        cy.get(this.botonLinkedin).should('be.visible')
+        cy.get(this.botonTwitter).should('be.visible')
+    }
+
+}
+
+export const homepage = new homePage()
